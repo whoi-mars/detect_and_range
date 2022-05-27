e7885510986a85fa2364c8b4f622df0f8cdfdd1a
Mark Goldwater <goldwater243@gmail.com>
2022-05-27 14:09:47 +0000

    Cleanup

--- dataMaker.py
+++ dataMaker.py
@@ -140,7 +140,7 @@ class DataHandler():
     def normalize_wav(self):
         self.p_t_noise = (self.p_t_noise - self.p_t_noise.mean(axis=0)) / self.p_t_noise.std(axis=0)
 
-    def create_spectrograms(self, fs=None, rand_shift=False, nperseg=31, noverlap=23, nfft=500, verbose=False, channels=1, size=None, env_ref=None):
+    def create_spectrograms(self, fs=None, rand_shift=False, nperseg=31, noverlap=23, nfft=500, verbose=False):
         
         """
         Create spectrograms using dispersive calls from KRAKEN simulation. 
@@ -154,9 +154,6 @@ class DataHandler():
         noverlap: int, number of samples overlap between windows.
         nfft: int, number of frequency 'bins' on the frequency axis of the spectrogram.
         verbose: bool, print example number as spectrograms are calculated.
-        channels: int, number of channels per example. If it is > 1, then the noverlap values from
-                  the variable noverlap_list within the function are used.
-        resize: tuple, height and width of the desired shape to resize the raw spectrogram size to.
 
         Returns
         -------
@@ -172,12 +169,6 @@ class DataHandler():
 
         if self.mode == 'wav' and rand_shift == True:
             raise ValueError("Randome shift not supported for exerimental data.")
-        
-        if size is not None:
-            resize_transform = transforms.Compose([transforms.Resize(size, antialias=True)])
-
-        nperseg_list = [nperseg, (3/4)*nperseg, (1/2)*nperseg]
-        #noverlap_list = [nperseg-1, (2/3)*nperseg, (1/3)*nperseg]
 
         if channels > len(nperseg_list):
             raise ValueError("Number of channels desired is greater than specified noverlaps in the function's 'noverlap_list' variable.")
@@ -185,225 +176,37 @@ class DataHandler():
         if size is None:
             # Generate one spectrogram to get the dimensions for the current settings
             [_,_,Zxx] = stft(x=self.p_t_noise[:,0], fs=fs, nperseg=nperseg, noverlap=noverlap, nfft=nfft)
-            self.X = np.zeros((self.n_examples, channels, Zxx.shape[0], Zxx.shape[1]), dtype='float32')
+            self.X = np.ones((self.n_examples, channels, Zxx.shape[0], Zxx.shape[1]), dtype='float32')
         else:
             self.X = np.zeros((self.n_examples, channels, size[0], size[1]), dtype='float32')
         self.y = self.labels
 
         for sig in tqdm(range(self.n_examples), disable=not verbose):
 
-            # Random wrapping using start/end time of call or just length of data if it is noise
-            if rand_shift and self.labels[4,sig]:
-                self.p_t_noise[:,sig] = self.__wrap_signal_random(self.p_t_noise[:,sig], self.t_dec_min[sig], self.t_dec_max[sig])
-            elif rand_shift and not self.labels[4,sig]:
-                self.p_t_noise[:,sig] = self.__wrap_signal_random(self.p_t_noise[:,sig])
+        # Random wrapping using start/end time of call or just length of data if it is noise
+        if rand_shift and self.labels[4,sig]:
+            self.p_t_noise[:,sig] = self.__wrap_signal_random(self.p_t_noise[:,sig], self.t_dec_min[sig], self.t_dec_max[sig])
+        elif rand_shift and not self.labels[4,sig]:
+            self.p_t_noise[:,sig] = self.__wrap_signal_random(self.p_t_noise[:,sig])
 
-            if channels > 1:
-                for c in range(channels):
-                    [f, t, Zxx] = stft(x=self.p_t_noise[:,sig], fs=fs, nperseg=nperseg_list[c], noverlap=nperseg_list[c]-1, nfft=nfft)
-                    log_spec = 10*np.log10(np.abs(Zxx)**2)
-                    log_spec = log_spec[np.newaxis, np.newaxis, :, :]
-                    if size is not None:
-                        log_spec = resize_transform(torch.from_numpy(log_spec)).numpy()
-                    log_spec = np.flip(log_spec, axis=2)
+            [f, t, Zxx] = stft(x=self.p_t_noise[:,sig], fs=fs, nperseg=nperseg, noverlap=noverlap, nfft=nfft)
+            log_spec = np.flipud(10*np.log10(np.abs(Zxx)**2))
+            if size is not None:
+                log_spec = resize(log_spec, size, anti_aliasing=True)
+            
+            # Save data
+            log_spec = log_spec[np.newaxis, :,:]
+            self.X[sig,:,:,:] = log_spec
 
-                    # Save data
-                    self.X[sig,c,:,:] = log_spec
-            elif channels == 1:
-                [f, t, Zxx] = stft(x=self.p_t_noise[:,sig], fs=fs, nperseg=nperseg, noverlap=noverlap, nfft=nfft)
-                log_spec = np.flipud(10*np.log10(np.abs(Zxx)**2))
-                if size is not None:
-                    log_spec = resize(log_spec, size, anti_aliasing=True)
-                
-                # Save data
-                log_spec = log_spec[np.newaxis, :,:]
-                self.X[sig,:,:,:] = log_spec
-            else:
-                raise ValueError("Channels must be between 1 and {}".format(len(nperseg_list)))
-
+            if sig == 10:
+                break
         # Normalize between 0 and 1
         # self.X = (self.X - self.X.min(axis=(2,3), keepdims=True)) / (self.X.max(axis=(2,3), keepdims=True) - self.X.min(axis=(2,3), keepdims=True))
-        # 5.6858e-04
-
-        if env_ref is not None:
-            for sig in range(self.n_examples):
-                for c in range(channels):
-                    self.X[sig,c,...] = exposure.match_histograms(self.X[sig,c,...], env_ref[c,...])
 
         # Make first dimension the number of examples
         self.y = self.y.T.astype('float32')
 
         return self.X, self.y, f, t
-
-    def create_spectrograms_complex_channels(self, fs=None, rand_shift=False, nperseg=31, noverlap=23, nfft=500, verbose=False, channels=1, size=None, env_ref=None):
-        
-        """
-        Create spectrograms using dispersive calls from KRAKEN simulation. 
-        TODO: Specify number of files or max file memory.
-
-        Parameters
-        ----------
-        fs: float, sampling frequencey used in KRAKEN simulation.
-        rand_shift: bool, whether or not to apply a random wrapping shift to the spectrogram.
-        nperseg: int, number of samples per window.
-        noverlap: int, number of samples overlap between windows.
-        nfft: int, number of frequency 'bins' on the frequency axis of the spectrogram.
-        verbose: bool, print example number as spectrograms are calculated.
-        channels: int, number of channels per example. If it is > 1, then the noverlap values from
-                  the variable noverlap_list within the function are used.
-        resize: tuple, height and width of the desired shape to resize the raw spectrogram size to.
-
-        Returns
-        -------
-        X: array-like, calculated spectrograms of shape (n_examples, Zxx.shape[0], Zxx.shape[1]).
-        y: array-like, labels of shape (n_labels, n_examples).
-        """
-
-        if fs is None:
-            fs = self.fs
-
-        if self.mode == 'wav' and rand_shift == True:
-            raise ValueError("Randome shift not supported for exerimental data.")
-        
-        resize_transform = transforms.Compose([transforms.Resize(size, antialias=True)])
-
-        if size is None:
-            # Generate one spectrogram to get the dimensions for the current settings
-            [_,_,Zxx] = stft(x=self.p_t_noise[:,0], fs=fs, nperseg=nperseg, noverlap=noverlap, nfft=nfft)
-            self.X = np.zeros((self.n_examples, channels, Zxx.shape[0], Zxx.shape[1]), dtype='float32')
-        else:
-            self.X = np.zeros((self.n_examples, channels, size[0], size[1]), dtype='float32')
-        self.y = self.labels
-
-        for sig in tqdm(range(self.n_examples), disable=not verbose):
-
-            # Random wrapping using start/end time of call or just length of data if it is noise
-            if rand_shift and self.labels[4,sig]:
-                self.p_t_noise[:,sig] = self.__wrap_signal_random(self.p_t_noise[:,sig], self.t_dec_min[sig], self.t_dec_max[sig])
-            elif rand_shift and not self.labels[4,sig]:
-                self.p_t_noise[:,sig] = self.__wrap_signal_random(self.p_t_noise[:,sig])
-
-            [f, t, Zxx] = stft(x=self.p_t_noise[:,sig], fs=fs, nperseg=nperseg, noverlap=nperseg_list[c]-1, nfft=nfft)
-            log_spec = 10*np.log10(np.abs(Zxx)**2)
-            log_spec = log_spec[np.newaxis, np.newaxis, :, :]
-            real_vals = Zxx.real
-            imag_vals = Zxx.imag
-            if size is not None:
-                log_spec = resize_transform(torch.from_numpy(log_spec)).numpy()
-                real_vals = resize_transform(torch.from_numpy(real_vals)).numpy()
-                imag_vals = resize_transform(torch.from_numpy(imag_vals)).numpy()
-            log_spec = np.flip(log_spec, axis=2)
-
-            # Save data
-            self.X[sig,0,...] = log_spec
-            self.X[sig,1,...] = real_vals
-            self.X[sig,2,...] = imag_vals
-
-        # Normalize between 0 and 1
-        self.X = (self.X - self.X.min(axis=(2,3), keepdims=True)) / (self.X.max(axis=(2,3), keepdims=True) - self.X.min(axis=(2,3), keepdims=True))
-
-        if env_ref is not None:
-            for sig in range(self.n_examples):
-                for c in range(channels):
-                    self.X[sig,c,...] = exposure.match_histograms(self.X[sig,c,...], env_ref[c,...])
-
-        # Make first dimension the number of examples
-        self.y = self.y.T.astype('float32')
-
-        return self.X, self.y, f, t
-
-    def create_melspectrograms(self, fs=None, rand_shift=False, nperseg=31, noverlap=23, nfft=500, verbose=False, channels=1, size=None, env_ref=None):
-        
-        """
-        Create spectrograms using dispersive calls from KRAKEN simulation. 
-        TODO: Specify number of files or max file memory.
-
-        Parameters
-        ----------
-        fs: float, sampling frequencey used in KRAKEN simulation.
-        rand_shift: bool, whether or not to apply a random wrapping shift to the spectrogram.
-        nperseg: int, number of samples per window.
-        noverlap: int, number of samples overlap between windows.
-        nfft: int, number of frequency 'bins' on the frequency axis of the spectrogram.
-        verbose: bool, print example number as spectrograms are calculated.
-        channels: int, number of channels per example. If it is > 1, then the noverlap values from
-                  the variable noverlap_list within the function are used.
-        resize: tuple, height and width of the desired shape to resize the raw spectrogram size to.
-
-        Returns
-        -------
-        X: array-like, calculated spectrograms of shape (n_examples, Zxx.shape[0], Zxx.shape[1]).
-        y: array-like, labels of shape (n_labels, n_examples).
-        """
-
-        # For 3 channel nperseg = 60, noverlap = [59, 40, 20], nfft = 447
-        # For 1 channel nperseg = 60, noverlap = 52, nfft = 750
-
-        if fs is None:
-            fs = self.fs
-
-        if self.mode == 'wav' and rand_shift == True:
-            raise ValueError("Randome shift not supported for exerimental data.")
-        
-        resize_transform = transforms.Compose([transforms.Resize(size, antialias=True)])
-
-        nperseg_list = [nperseg, (3/4)*nperseg, (1/2)*nperseg]
-        #noverlap_list = [nperseg-1, (2/3)*nperseg, (1/3)*nperseg]
-
-        if channels > len(nperseg_list):
-            raise ValueError("Number of channels desired is greater than specified noverlaps in the function's 'noverlap_list' variable.")
-
-        if size is None:
-            # Generate one spectrogram to get the dimensions for the current settings
-            [_,_,Zxx] = stft(x=self.p_t_noise[:,0], fs=fs, nperseg=nperseg, noverlap=noverlap, nfft=nfft)
-            self.X = np.zeros((self.n_examples, channels, Zxx.shape[0], Zxx.shape[1]), dtype='float32')
-        else:
-            self.X = np.zeros((self.n_examples, channels, size[0], size[1]), dtype='float32')
-        self.y = self.labels
-
-        for sig in tqdm(range(self.n_examples), disable=not verbose):
-
-            # Random wrapping using start/end time of call or just length of data if it is noise
-            if rand_shift and self.labels[4,sig]:
-                self.p_t_noise[:,sig] = self.__wrap_signal_random(self.p_t_noise[:,sig], self.t_dec_min[sig], self.t_dec_max[sig])
-            elif rand_shift and not self.labels[4,sig]:
-                self.p_t_noise[:,sig] = self.__wrap_signal_random(self.p_t_noise[:,sig])
-
-            if channels > 1:
-                for c in range(channels):
-                    Zxx = librosa.feature.melspectrogram(y=self.p_t_noise[:,sig], sr=fs, win_length=int(nperseg_list[c]), hop_length=1, n_fft=nfft, fmin=10, n_mels=16)
-                    log_spec = 10*np.log10(np.abs(Zxx)**2)
-                    log_spec = log_spec[np.newaxis, np.newaxis, :, :]
-                    if size is not None:
-                        log_spec = resize_transform(torch.from_numpy(log_spec)).numpy()
-                    log_spec = np.flip(log_spec, axis=2)
-
-                    # Save data
-                    self.X[sig,c,:,:] = log_spec
-            elif channels == 1:
-                [f, t, Zxx] = stft(x=self.p_t_noise[:,sig], fs=fs, nperseg=nperseg, noverlap=noverlap, nfft=nfft)
-                log_spec = np.flipud(10*np.log10(np.abs(Zxx)**2))
-                if size is not None:
-                    log_spec = resize(log_spec, size, anti_aliasing=True)
-                
-                # Save data
-                log_spec = log_spec[np.newaxis, :,:]
-                self.X[sig,:,:,:] = log_spec
-            else:
-                raise ValueError("Channels must be between 1 and {}".format(len(nperseg_list)))
-
-        # Normalize between 0 and 1
-        self.X = (self.X - self.X.min(axis=(2,3), keepdims=True)) / (self.X.max(axis=(2,3), keepdims=True) - self.X.min(axis=(2,3), keepdims=True))
-
-        if env_ref is not None:
-            for sig in range(self.n_examples):
-                for c in range(channels):
-                    self.X[sig,c,...] = exposure.match_histograms(self.X[sig,c,...], env_ref[c,...])
-
-        # Make first dimension the number of examples
-        self.y = self.y.T.astype('float32')
-
-        return self.X, self.y
 
     def __wrap_signal_random(self, x, t_min=None, t_max=None):
         
@@ -484,8 +287,8 @@ class DataHandler():
         top = self.X[X_train].shape[0] - remain
         with h5py.File(train_path, 'a') as f:
             i = 0
-            f.create_dataset("data", data=self.X[X_train][:i+c,...], chunks=(1,232,232), maxshape=(None,1,232,232))
-            f.create_dataset("labels", data=self.y[y_train][:i+c,...], chunks=True, maxshape=(None,5))
+            f.create_dataset("data", data=self.X[X_train][:i+c,...], chunks=(1,1,226,226), maxshape=(None,1,226,226))
+            f.create_dataset("labels", data=self.y[y_train][:i+c,...], chunks=(1,5), maxshape=(None,5))
             i += c
             while True:
                 if i == top:
@@ -508,8 +311,8 @@ class DataHandler():
         top = self.X[X_test].shape[0] - remain
         with h5py.File(test_path, 'a') as f:
             i = 0
-            f.create_dataset("data", data=self.X[X_test][:i+c,...], chunks=(1,232,232), maxshape=(None,1,232,232))
-            f.create_dataset("labels", data=self.y[y_test][:i+c,...], chunks=True, maxshape=(None,5))
+            f.create_dataset("data", data=self.X[X_test][:i+c,...], chunks=(1,1,226,226), maxshape=(None,1,226,226))
+            f.create_dataset("labels", data=self.y[y_test][:i+c,...], chunks=(1,5), maxshape=(None,5))
             i += c
             while True:
                 if i == top:
@@ -532,8 +335,8 @@ class DataHandler():
         top = self.X[X_val].shape[0] - remain
         with h5py.File(val_path, 'a') as f:
             i = 0
-            f.create_dataset("data", data=self.X[X_val][:i+c,...], chunks=(1,232,232), maxshape=(None,1,232,232))
-            f.create_dataset("labels", data=self.y[y_val][:i+c,...], chunks=True, maxshape=(None,5))
+            f.create_dataset("data", data=self.X[X_val][:i+c,...], chunks=(1,1,226,226), maxshape=(None,1,226,226))
+            f.create_dataset("labels", data=self.y[y_val][:i+c,...], chunks=(1,5), maxshape=(None,5))
             i += c
             while True:
                 if i == top:
