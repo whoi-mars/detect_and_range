e4b8c36837d39e771b7776401ec0c20f2fb9c24a
Mark Goldwater <goldwater243@gmail.com>
2022-07-16 14:49:52 +0000

    Add MTL loss approximation

--- losses.py
+++ losses.py
@@ -131,7 +131,7 @@ class UncertainSelectiveMSEAndClass(nn.Module):
 
         # Get classification loss
         c_loss = torch.exp(-self.log_vars[1])*outputs[torch.arange(len(outputs),dtype=torch.long), (c_labels.squeeze()+1).long()] - torch.log(torch.sum(torch.exp(torch.exp(-self.log_vars[1])*outputs[:,1:]),dim=1))
-    
+
         # Calculate range loss
         r_loss = self.MSE(call_outs.squeeze(), call_r_labels.squeeze())
         r_precision = torch.exp(-self.log_vars[0])
@@ -143,6 +143,54 @@ class UncertainSelectiveMSEAndClass(nn.Module):
         r_loss_m = r_loss_final.mean()
         c_loss_m = (-1*c_loss).mean()
         r_c_loss_m = (r_loss_final - c_loss).mean()
+        
+        return r_c_loss_m, r_loss_m, c_loss_m
+
+class UncertainSelectiveMSEAndClassApprox(nn.Module):
+    
+    """
+    Class for loss which adds binary cross entropy loss and MSE loss
+    for range predictions. The MSE loss only penalizes incorrect range
+    predictions for examples of class 1 (with a call in the spectrogram).
+    This loss also implements the approach from Kendall et al. (https://arxiv.org/abs/1705.07115) 
+    to learn weights for the classification and ranging tasks. 
+    """
+    
+    def __init__(self, device, log_var_list=None):
+        super().__init__()
+        self.MSE = nn.MSELoss(reduction='none')
+        # self.CE = nn.CrossEntropyLoss(reduction='none')
+
+        # Learned variables for weighting the tasks
+        if log_var_list is None:
+            self.log_vars = [torch.tensor(0., device=device, requires_grad=True), torch.tensor(0., device=device, requires_grad=True)]
+        else:
+            self.log_vars = [torch.tensor(log_var_list[0].item(), device=device, dtype=torch.float32, requires_grad=True), torch.tensor(log_var_list[1].item(), device=device, dtype=torch.float32, requires_grad=True)]
+
+    def forward(self, outputs, r_labels, c_labels):
+
+        # Get loss
+        sm = torch.nn.functional.softmax(outputs[:,1:],dim=1)
+        c_loss = -torch.log(sm[torch.arange(len(sm),dtype=torch.long), c_labels.squeeze().long()])
+        # c_loss2 = self.CE(outputs[:,1:], c_labels.squeeze())
+        
+        # Calculate weighted class loss
+        c_loss = torch.exp(-self.log_vars[1])*c_loss + 0.5*self.log_vars[1]
+
+        # Isolate ranges for call-containing example only
+        call_outs = outputs[c_labels.squeeze() == 1, 0]
+        call_r_labels = r_labels[c_labels.squeeze() == 1]
+        
+        # Calculate range loss
+        r_loss = self.MSE(call_outs.squeeze(), call_r_labels.squeeze())
+        r_loss = 0.5*(torch.exp(-self.log_vars[0])*r_loss + self.log_vars[0])
+        r_loss_final = torch.zeros_like(c_loss) 
+        r_loss_final[c_labels.squeeze() == 1] = r_loss
+
+        # Calculate means
+        r_c_loss_m = (r_loss_final + c_loss).mean()
+        c_loss_m = c_loss.mean()
+        r_loss_m = r_loss_final.mean()
         
         return r_c_loss_m, r_loss_m, c_loss_m
 
